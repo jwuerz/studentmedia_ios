3a97170a0edcde62bff3bc7970642010d4fe16f2
Daniela Beraldo <Dani@Danielas-MacBook-Pro.local>
2015-06-22 15:33:52 -0400

    Initial Commit

---
+++ ItsOnBlue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ItsOnBlue
+//
+//  Created by Daniela Beraldo on 6/22/15.
+//  Copyright (c) 2015 Daniela Beraldo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ItsOnBlue/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ItsOnBlue
+//
+//  Created by Daniela Beraldo on 6/22/15.
+//  Copyright (c) 2015 Daniela Beraldo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
